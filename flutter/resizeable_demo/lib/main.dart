b1e90938f96307c5aa77d5b1e9d7dfc6e00e390b
Pete Giardiniere <tul49035@temple.edu>
2020-06-16 16:55:27 -0400

    demo works with live camera preview

--- flutter/resizeable_demo/lib/main.dart
+++ flutter/resizeable_demo/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:camera/camera.dart';
 import 'package:resizeable_demo/resize_widget.dart';
 
-void main() {
+List<CameraDescription> cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(MyApp());
 }
 
@@ -17,12 +22,12 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         // body: Demo(),
-        body: Demo2(),
+        // body: Demo2(),
+        body: Demo3(),
       ),
     );
   }
 }
-
 
 // Demo implements features via the code sample derived ResizableWidget.
 class Demo extends StatefulWidget {
@@ -41,7 +46,9 @@ class _DemoState extends State<Demo> {
           ResizableWidget(
             child: FittedBox(
               fit: BoxFit.fill,
-              child: Image.asset('assets/images/stencil_art_transparent.png',),
+              child: Image.asset(
+                'assets/images/stencil_art_transparent.png',
+              ),
             ),
           ),
         ],
@@ -50,20 +57,74 @@ class _DemoState extends State<Demo> {
   }
 }
 
-// Demo2 implements features via the photo_view package.
+// Demo2 implements features via the PhotoView package.
+// Due to issues down the line with gesture capturing, it doesn't do it all.
 class Demo2 extends StatefulWidget {
   @override
   _Demo2State createState() => _Demo2State();
 }
 
-class _Demo2State extends State<Demo2>{
+class _Demo2State extends State<Demo2> {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(40),
+      child: Stack(children: <Widget>[
+        PhotoView(
+          imageProvider: AssetImage('assets/images/liquid.jpg'),
+        ),
+      ]),
+    );
+  }
+}
+
+// Demo3 implements features via the code sample derived ResizableWidget
+// It also overlays the reticle on a live camera preview, not a static image.
+class Demo3 extends StatefulWidget {
+  @override
+  _Demo3State createState() => _Demo3State();
+}
+
+class _Demo3State extends State<Demo3> {
+  CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) return;
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+
+    return Container(
+      padding: EdgeInsets.only(top: 40),
       child: Stack(
         children: <Widget>[
-          PhotoView(imageProvider: AssetImage('assets/images/liquid.jpg'),),
+          AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: CameraPreview(controller),
+          ),
+
+          ResizableWidget(
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: Image.asset('assets/images/stencil_art_transparent.png',),
+            ),
+          ),
         ]
       ),
     );
